b67341f63adcfcd688b6844814eadfd826d031ca
GBielke25 <831366@mystma.org>
2023-03-29 15:04:17 -0500

    2

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 def hasL(w):
     num = 0
     for i in range(0, len("l")):
-        if hasL[i] == "l":
-            num = num + 1
-            print("True")
-    
+        if w == "l":
+            print(True)
         else:
-            print("False")
+            print(False)
+    
