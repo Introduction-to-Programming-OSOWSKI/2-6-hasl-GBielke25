dc0460f7538a5680d206a2b2cac8deea556daa7b
GBielke25 <831366@mystma.org>
2023-03-29 15:12:04 -0500

    3

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def hasL(w):
     num = 0
-    for i in range(0, len("l")):
-        if w == "l":
-            print(True)
-        else:
-            print(False)
+    for i in range(0, len(w)):
+        if w[i] == "l":
+            return True
+    return False
     
+hasL("alabama")
